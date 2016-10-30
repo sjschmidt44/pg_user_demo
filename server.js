74485134c9fc3afe3f7d08444356ccc14d768720
Scott Schmidt <sjschmidt44@gmail.com>
2016-10-29 17:37:54 -0700

    update server with arrow functions

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const pg = require('pg')
 const express = require('express')
@@ -10,95 +10,79 @@ app.get('/api/users', (req, res) => {
   const client = new pg.Client(conString)
 
   client.connect(err => {
-    if (err) console.error('could not connect to postgres', err)
+    if(err) console.error('could not connect to postgres', err)
 
     client.query('SELECT * FROM users', (err, result) => {
       if(err) console.error('error running query', err)
-      res.send(result);
-      client.end();
-    });
-  });
-});
+      res.send(result)
+      client.end()
+    })
+  })
+})
 
-app.all('/api/users/add', function(req, res) {
-  const client = new pg.Client(conString);
+app.all('/api/users/add', (req, res) => {
+  const client = new pg.Client(conString)
 
-  client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
+  client.connect(err => {
+    if(err) console.error('could not connect to postgres', err)
+
     client.query(
       'INSERT INTO users (name, age, sex) VALUES($1, $2, $3)',
       [req.query.name, req.query.age, req.query.sex],
-      function(err, result) {
-        if(err) {
-          return console.error('error running query', err);
-        }
-      client.end();
-    });
-  });
-  res.redirect('/');
+      err => {
+        if(err) console.error('error running query', err)
+        client.end()
+      })
+  })
+  res.redirect('/')
 })
 
-app.all('/api/users/update', function(req, res) {
-  const client = new pg.Client(conString);
-  console.log(req.query);
+app.all('/api/users/update', (req, res) => {
+  const client = new pg.Client(conString)
 
-  client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
+  client.connect(err => {
+    if(err) console.error('could not connect to postgres', err)
+
     client.query(
       'UPDATE users SET name = $1, age = $2, sex = $3 WHERE id = $4',
       [req.query.name, req.query.age, req.query.sex, req.query.id],
-      function(err, result) {
-        if(err) {
-          return console.error('error running query', err);
-        }
-      client.end();
-    });
-  });
-  res.redirect('/');
+      err => {
+        if(err) console.error('error running query', err)
+        client.end()
+      })
+  })
+  res.redirect('/')
 })
 
-app.get('/api/users/delete', function(req, res) {
-  const client = new pg.Client(conString);
+app.get('/api/users/delete', (req, res) => {
+  const client = new pg.Client(conString)
 
-  client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query('DELETE FROM users WHERE id=' + req.query.id, function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      client.end();
-    });
-  });
-  res.redirect('/');
+  client.connect(err => {
+    if(err) console.error('could not connect to postgres', err)
+    client.query('DELETE FROM users WHERE id=' + req.query.id, (err) => {
+      if(err) console.error('error running query', err)
+      client.end()
+    })
+  })
+  res.redirect('/')
 })
 
-app.get('/', function(req, res) {
-  const client = new pg.Client(conString);
+app.get('/', (req, res) => {
+  const client = new pg.Client(conString)
 
   client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
+    if(err) console.error('could not connect to postgres', err)
+
     client.query(
       'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT NOT NULL, age INT NOT NULL, sex TEXT NOT NULL)',
-      function(err, result) {
-        if(err) {
-          return console.error('error running query', err);
-        }
-      client.end();
-    });
-  });
-  res.sendFile(__dirname + '/public/index.html');
-});
+      err => {
+        if(err) console.error('error running query', err)
+        client.end()
+      })
+  })
+  res.sendFile(__dirname + '/public/index.html')
+})
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 
-app.listen(port, function() {
-  console.log('Server started on port ' + port + '!');
-});
+app.listen(port, () => console.log(`Server listening on port ${port}`))
